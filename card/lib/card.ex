6a3daca26631e73331e8134f7dd29cba0b461aac
shinikhee <ikheee@naver.com>
2023-06-20 16:25:42 +0900

    test

---
+++ card/lib/card.ex
@@ -0,0 +1,23 @@
+defmodule Card do
+  @moduledoc """
+  Documentation for `Card`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Card.hello()
+      :world
+
+  """
+  def hello do
+    # :world
+    "hi there!"
+  end
+  def hello_array do
+    # :world
+    ["hi", "there", "1"]
+  end
+end
